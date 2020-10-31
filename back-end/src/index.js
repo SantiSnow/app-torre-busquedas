b060f6f1fda609efeb28d80e4d5792c016ad0cfd
santiago aguirre perez <aguirresantiago81@gmail.com>
2020-10-30 23:40:55 -0300

    Commit inicial del proyecto, front-end en construccion, la api responde correctamente

---
+++ back-end/src/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var request = require('request'); 
+const cors = require('cors');
+
+var app = express();
+
+
+/* CORS ENABLE */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+    app.options('*', (req, res) => { 
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+/* CORS ENABLE */
+
+
+//rutas
+app.get('/', function (req, res) {
+    res.json(
+        { "Mensaje": "Bienvenido"}
+    );
+});
+
+app.get('/api', function(req, res){
+    request("https://bio.torre.co/api/bios/torrenegra", function(err, body){
+        res.send(body);
+    });
+});
+
+app.get('/api/buscar', function(req, res){
+    var id = req.body.id;
+    request("https://torre.co/api/opportunities/:id", function(err, body){
+        res.send(body);
+    });
+});
+
+app.get('/api/buscar', function(req, res){
+    var username = req.body.username;
+    request("https://torre.bio/api/bios/:username", function(err, body){
+        res.send(body);
+    });
+});
+
+//get
+//https://torre.co/api/opportunities/$id 
+//https://torre.bio/api/bios/$username
+
+
+/* post
+
+ https://search.torre.co/opportunities/_search/?[offset=$offset&size=$size&aggregate=$aggregate] 
+
+ https://search.torre.co/people/_search/?[offset=$offset&size=$size&aggregate=$aggregate]
+ */
+
+app.listen(3000);
